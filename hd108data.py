3060ec806f33d5ad4e100d1879582f8ee0e151ac
Tinic Uro <tinicuro@gmail.com>
2021-11-09 05:06:47 +0000

    WIP

--- hd108data.py
+++ hd108data.py
@@ -7,6 +7,10 @@ import busio
 import spidev
 import adafruit_tsl2591
 
+spi = spidev.SpiDev()
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_tsl2591.TSL2591(i2c)
+
 def leddat(rvalue, gvalue, bvalue, rgain, ggain, bgain):
     bytes = [];
     for x in range(16):
@@ -23,18 +27,21 @@ def leddat(rvalue, gvalue, bvalue, rgain, ggain, bgain):
         bytes.append( (bvalue >> 0 ) & 0xFF)
     return bytes
 
-def lux(spi, sensor, rvalue, gvalue, bvalue, rgain, ggain, bgain):
-    lux = 0
-    # wait until everything is completely dark
+def settleToBlack():
     while True:
-        spi.writebytes2(leddat(0, 0, 0, rgain, ggain, bgain))
+        spi.writebytes2(leddat(0, 0, 0, 0, 0, 0))
         lux = sensor.lux
         if lux == 0:
             break
-    # wait until we stabilize
+
+def getLux(rvalue, gvalue, bvalue, rgain, ggain, bgain, settleTime):
+    lux = 0
+    # wait until everything is completely dark
+    settleToBlack()
     while True:
         spi.writebytes2(leddat(rvalue, gvalue, bvalue, rgain, ggain, bgain))
-        time.sleep(0.2)
+        # wait until we stabilize
+        time.sleep(settleTime)
         lux = sensor.lux
         if (lux != 0):
             break
@@ -42,36 +49,29 @@ def lux(spi, sensor, rvalue, gvalue, bvalue, rgain, ggain, bgain):
 
 def main():
 
-    spi = spidev.SpiDev()
     spi.open(0,0)
     spi.bits_per_word = 8
     spi.max_speed_hz = 4000000
 
-    i2c = busio.I2C(board.SCL, board.SDA)
-    sensor = adafruit_tsl2591.TSL2591(i2c)
-
-    sensor.gain = 0x10
+    sensor.gain = 0x20
     sensor.integration_time = 0x0
-
-    sleep_time = 1
 
     rgain = 31
     ggain = 31
     bgain = 31
 
-    f = open("hd108data_gain{}_int{}_rg{}_gg{}_bg{}.txt".format(sensor.gain,sensor.integration_time,rgain,ggain,bgain), "w")
+    f = open("hd108data_g{}i{}rg{}gg{}bg{}.txt".format(sensor.gain>>4,sensor.integration_time,rgain,ggain,bgain), "w")
 
-    for i in range(16):
-        spi.writebytes2(leddat(0, 0, 0, rgain, ggain, bgain))
-        time.sleep(0.1)
-        sensor.lux
+    settleToBlack()
 
-    for i in range(3,65536,1):
-        rlux = lux(spi,sensor,i,0,0,rgain, ggain, bgain)
-        glux = lux(spi,sensor,0,i,0,rgain, ggain, bgain)
-        blux = lux(spi,sensor,0,0,i,rgain, ggain, bgain)
+    # first section with highest gain
+    for i in range(1,1024,1):
+        rlux = getLux(i, 0, 0, rgain, ggain, bgain, 0.2)
+        glux = getLux(0, i, 0, rgain, ggain, bgain, 0.2)
+        blux = getLux(0, 0, i, rgain, ggain, bgain, 0.2)
         print("i:{} r:{:0.5f} g:{:0.5f} b{:0.5f}".format(i, rlux, glux, blux))
         f.write("{},{:0.5f},{:0.5f},{:0.5f}\n".format(i, rlux, glux, blux))
+        f.flush()
     
     f.close()
 
