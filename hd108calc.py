9f86b00e0ae91a5256c7e266651bcd8a79298bdd
Tinic Uro <tinicuro@gmail.com>
2021-11-09 21:20:23 -0800

    Figure monotone curve map.

--- hd108calc.py
+++ hd108calc.py
@@ -23,6 +23,10 @@ ri = []
 gi = []
 bi = []
 
+rc = []
+gc = []
+bc = []
+
 '''
 -a * exp(-b * 1) + a = 1
 
@@ -59,13 +63,13 @@ def bifunc(x, b):
 
 def main():
 
-    with open('hd108data_g0i0rg31gg31bg31.txt', newline='\n') as csvfile:
+    with open('hd108data_g2i0rg31gg31bg31_0-1023.txt', newline='\n') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in csvreader:
             ra.append(float(row[1]))
             ga.append(float(row[2]))
             ba.append(float(row[3]))
-            
+
     rmax = max(ra)
     gmax = max(ga)
     bmax = max(ba)
@@ -95,9 +99,34 @@ def main():
     for i in np.arange(0, 1, 1/256):
         bi.append(bifunc(i, bconst))
 
-    plt.plot(ra, color='salmon')
-    plt.plot(ga, color='limegreen')
-    plt.plot(ba, color='royalblue')
+    rc = ra.copy()
+    gc = ga.copy()
+    bc = ba.copy()
+
+    # this correction map makes sure that
+    # the response curve is monotone
+    monotone_map = [
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+        0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+        0x0F, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15,
+        0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D
+    ]
+
+    for i in range(len(ra)):
+        rc[i] = ra[monotone_map[(i&0x1F)]+(i&(~0x1F))]
+        gc[i] = ga[monotone_map[(i&0x1F)]+(i&(~0x1F))]
+        bc[i] = ba[monotone_map[(i&0x1F)]+(i&(~0x1F))]
+
+    plt.grid(True,'both','both')
+    plt.xticks(np.arange(-1, 1023, step=16))
+
+#    plt.plot(ra, color='salmon')
+#    plt.plot(ga, color='limegreen')
+#    plt.plot(ba, color='royalblue')
+
+    plt.plot(rc, color='darkred')
+    plt.plot(gc, color='forestgreen')
+    plt.plot(bc, color='darkblue')
 
 #    plt.plot(rq, color='darkred')
 #    plt.plot(gq, color='forestgreen')
